06bb546e28ff3bca6fed1ce3f74299d270f1a433
Love Trivedi <lovelyjoshi93@gmail.com>
2019-02-19 19:05:37 +0530

    removed unwanted code

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,32 +1,5 @@
-/*container will go here and all the common components like header footer sidebar should be here 
-*/
 import React from 'react'
-
 import Header from './header'
-
-const leaderBoardList = [
-    {
-        id: 1,
-        name: 'Manoj Singh Negi',
-        commits: 1,
-        completed: 1,
-        points: 5
-    },
-    {
-        id: 2,
-        name: 'Manoj Singh Negi',
-        commits: 1,
-        completed: 1,
-        points: 5
-    },
-    {
-        id: 3,
-        name: 'Manoj Singh Negi',
-        commits: 1,
-        completed: 1,
-        points: 5
-    },
-]
 
 const LeaderBoard = (props) => {
     return (
