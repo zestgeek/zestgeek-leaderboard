4443ec21150c34085cb9fcaad41c88dd233bf969
Love Trivedi <lovelyjoshi93@gmail.com>
2019-02-20 14:06:46 +0530

    resolved code consistency and indentations issue

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Header from './header'
 
-const LeaderBoard = (props) => {
-    return (
+const LeaderBoard = props => 
         <div className="container">
             <Header />
             {props.children}
         </div>
-    )
-}
 
 export default LeaderBoard
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const Header = () =>{
-	return (
+const Header = () =>
 	 <div className="columns is-centered">
             <div className="column has-text-centered">
                 <h1 className="title">Zestgeek Leader board</h1>
             </div>
         </div>
-        )
-}
+
 export default Header;
