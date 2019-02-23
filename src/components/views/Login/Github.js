05564273ac65690d1ef001a31f587ad76063b98d
Love Trivedi <lovelyjoshi93@gmail.com>
2019-02-23 15:47:35 +0530

    fixed indentations

--- src/components/views/Login/Github.js
+++ src/components/views/Login/Github.js
@@ -6,11 +6,11 @@ const onFailure = response => console.error(response);
 
 const Github = () =>
   <GitHubLogin 
-  	clientId="c82bc163208f51dc7324"
-  	redirectUri="http://localhost:3000"
-    onSuccess={onSuccess}
-    onFailure={onFailure}
-    className="button"
+ 		clientId="c82bc163208f51dc7324"
+ 		redirectUri="http://localhost:3000"
+ 		onSuccess={onSuccess}
+ 		onFailure={onFailure}
+ 		className="button"
   />
 
 export default Github;
